be4a86115c8145cf3e262e3715a2d213c8ed63f7
Crowdbotics <team@crowdbotics.com>
2022-01-16 18:21:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gpg_wallet_32880",
+  url: "https://gpg_wallet_32880.botics.co",
+  api: "https://gpg_wallet_32880.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
